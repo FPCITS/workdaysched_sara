f9f1ed60cab18bacc0a760408c3c060708ab98f2
FPCITS <sara@freedompracticecoaching.com>
2022-06-06 00:02:58 -0500

    added js

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,99 @@
-var m = entry();
+var m = moment();
 
-$("#currentDay").text(entry().format('LLLL'));
+$("#currentDay").text(moment().format('LLLL'));
 
-$(document).ready(function() {
+$(document).ready( function() {
     colorChange ();
-    WebGL2RenderingContext();
+    renderText();
 });
 
 function colorChange () {
 
-    var currentTime = entry().hours();
-    console.log("Current Time + currentTime");
+    var currentTime = moment().hours();
+    console.log("Current Time" + currentTime);
 
-    $(.input").each(function() {
+    $(".input").each(function() {
         var scheduledTime = parseInt($(this).attr("id"))
         console.log(scheduledTime);
 
         if (currentTime > scheduledTime) {
             $(this).removeClass("future");
-            $(this).removeClass("past");
+            $(this).addClass("past");
             $(this).removeClass("present");
         } else if (currentTime < scheduledTime){
-            $(this).removeClass("future");
+            $(this).addClass("future");
             $(this).removeClass("past");
             $(this).removeClass("present");
         } else {
             $(this).removeClass("future");
             $(this).removeClass("past");
-            $(this).removeClass("present");
+            $(this).addClass("present");
         }
 });
 }
 
-var eventText;
-var eventTime;
+var entryText;
+var entryTime;
 
-$("saveBtn")
+$(".saveBtn").click(function() {
+    entryText = $(this).siblings(".input").val();
+    console.log(entryText);xa
+    entryTime = $(this).siblings(".hour").text();
+    console.log(entryTime);
+    localStorage.setItem(entryTime, JSON.stringify(entryText));
+
+    colorChange ();
+    renderText ();
+
+
+});
+
+$(".deleteBtn").click(function() {
+    entryText = $(this).siblings(".input").val("");
+    entryText = $(this).siblings(".input").val();
+    entryTime = $(this).siblings(".hour").text();
+    localStorage.setItem(entryTime, JSON.stringify(entryText));
+
+colorChange ();
+renderText ();
+
+});
+
+function renderText() {
+    var saveEntryText9 = JSON.parse(localStorage.getItem("8:00 am"));
+    $("#9").val("");
+    $("#9").val(saveEntryText9);
+
+    var saveEntryText10 = JSON.parse(localStorage.getItem("8:00 am"));
+    $("#10").val("");
+    $("#10").val(saveEntryText10);
+
+    var saveEntryText11 = JSON.parse(localStorage.getItem("8:00 am"));
+    $("#11").val("");
+    $("#11").val(saveEntryText11);
+
+    var saveEntryText12 = JSON.parse(localStorage.getItem("8:00 am"));
+    $("#12").val("");
+    $("#12").val(saveEntryText12);
+
+    var saveEntryText13 = JSON.parse(localStorage.getItem("8:00 am"));
+    $("#13").val("");
+    $("#13").val(saveEntryText13);
+
+    var saveEntryText14 = JSON.parse(localStorage.getItem("8:00 am"));
+    $("#14").val("");
+    $("#14").val(saveEntryText14);
+
+    var saveEntryText15 = JSON.parse(localStorage.getItem("8:00 am"));
+    $("#15").val("");
+    $("#15").val(saveEntryText15);
+
+    var saveEntryText16 = JSON.parse(localStorage.getItem("8:00 am"));
+    $("#16").val("");
+    $("#16").val(saveEntryText16);
+
+    var saveEntryText17 = JSON.parse(localStorage.getItem("8:00 am"));
+    $("#17").val("");
+    $("#17").val(saveEntryText17);
+
+};
